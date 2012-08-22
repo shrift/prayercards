eaab4d51d7c4d93a4a8ab49d1573b62af790fef3
Brendan Martens <brendan.martens@crosscomm.net>
2012-08-22 16:32:47 -0400

    Move menu items into overflow menu.

--- src/com/bubbletastic/prayercards/TheDivineHourActivity.java
+++ src/com/bubbletastic/prayercards/TheDivineHourActivity.java
@@ -9,7 +9,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.ImageButton;
 
 public class TheDivineHourActivity extends FragmentActivity {
 
