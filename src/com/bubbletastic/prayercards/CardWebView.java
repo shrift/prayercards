4b0187eb009b839ac3a5b01fd65f004f3b4852db
Brendan Martens <brendan.martens@crosscomm.net>
2012-08-31 08:33:18 -0400

    Get almost everything working for 2.2 and above now.
    The only thing missing is the list rows don't stay highlighted in 2.2
    like they do in 4.0. >:(

--- src/com/bubbletastic/prayercards/CardWebView.java
+++ src/com/bubbletastic/prayercards/CardWebView.java
@@ -1,8 +1,14 @@
 package com.bubbletastic.prayercards;
 
+import java.lang.reflect.Field;
+
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.os.Build;
 import android.util.AttributeSet;
+import android.view.View;
 import android.webkit.WebView;
+import android.widget.ZoomButtonsController;
 
 import com.bubbletastic.prayercards.model.Card;
 
@@ -19,23 +25,48 @@ public class CardWebView extends WebView {
 	public CardWebView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
 	}
-//	public CardWebView(Context context, AttributeSet attrs, int defStyle, boolean privateBrowsing) {
-//		super(context, attrs, defStyle, privateBrowsing);
-//	}
 	public CardWebView(Context context, Card card) {
 		super(context);
 		this.card = card;
 		init();
 	}
 	
+	@SuppressLint("NewApi")
 	@SuppressWarnings("deprecation")
 	private void init() {
 		setBackgroundColor(getResources().getColor(android.R.color.white));
     	setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT, 0, 0));
     	getSettings().setSupportZoom(true);
     	getSettings().setBuiltInZoomControls(true);
-    	getSettings().setDisplayZoomControls(false);
+    	if (PrayerCards.DEVICE_SDK_VERSION >= Build.VERSION_CODES.HONEYCOMB) {
+    		getSettings().setDisplayZoomControls(false);
+    	} else {
+    		setZoomControlGone(this);
+    	}
 		loadUrl("file:///android_asset/"+card.fileName);
+	}
+	
+	private void setZoomControlGone(View view){
+	    Class<?> classType;
+	    Field field;
+	    try {
+	        classType = WebView.class;
+	        field = classType.getDeclaredField("mZoomButtonsController");
+	        field.setAccessible(true);
+	        ZoomButtonsController mZoomButtonsController = new ZoomButtonsController(view);
+	        mZoomButtonsController.getZoomControls().setVisibility(View.GONE);
+	        try {
+	            field.set(view, mZoomButtonsController);
+	        } catch (IllegalArgumentException e) {
+	            e.printStackTrace();
+	        } catch (IllegalAccessException e) {
+	            e.printStackTrace();
+	        }
+	    } catch (SecurityException e) {
+	        e.printStackTrace();
+	    } catch (NoSuchFieldException e) {
+	        e.printStackTrace();
+	    }
 	}
 	
 	public int getCardId() {
--- src/com/bubbletastic/prayercards/PrayerCards.java
+++ src/com/bubbletastic/prayercards/PrayerCards.java
@@ -1,16 +1,18 @@
 package com.bubbletastic.prayercards;
 
-import com.bubbletastic.prayercards.model.CardTitles;
-
 import android.app.Application;
+
+import com.bubbletastic.prayercards.model.CardTitles;
 
 public class PrayerCards extends Application {
 
 	public static CardTitles cardTitles;
+	public static Integer DEVICE_SDK_VERSION;
 
 	@Override
 	public void onCreate() {
 		super.onCreate();
+		DEVICE_SDK_VERSION = Integer.valueOf(android.os.Build.VERSION.SDK_INT);
 		cardTitles = new CardTitles(this);
 	}
 }
